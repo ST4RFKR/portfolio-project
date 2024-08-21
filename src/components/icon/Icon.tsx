dada3e39c138730fc0d04f93b3a028220a3b2850
Евгений Супруненко <admin@MacBook-Pro-Evgenij.local>
2024-08-21 18:08:20 +0300

    animate logo

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -6,14 +6,15 @@ type IconPropsType = {
     urlIcon: string,
     width?: string,
     height?: string,
-    viewBox?: string
+    viewBox?: string,
 }
 
 export function Icon(props: IconPropsType) {
     const {width, height, viewBox, urlIcon} = props
 
     return (
-        <svg width={width || '38'}
+        <svg
+            width={width || '38'}
              height={height || '38'}
              viewBox={viewBox || '0 0 38 38'}
              xmlns="http://www.w3.org/2000/svg">
--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import {Icon} from "../icon/Icon";
+import styled, {keyframes} from "styled-components";
 
+const float = keyframes`
+    0% {
+        transform: translate(0, 0);
+    }
+    50% {
+        transform: translate(10px ,10px);
+
+    }
+    100% {
+        transform: translate(0, 0);
+    }
+`;
 
 export function Logo() {
     return (
-        <a href="#">
-            <Icon width={'50'} height={'50'} viewBox={'0 0 50 50'} urlIcon={'logo'}/>
-        </a>
+        <StyledLinks href="#">
+            <Icon
+                width={'50'} height={'50'} viewBox={'0 0 50 50'} urlIcon={'logo'}/>
+        </StyledLinks>
     );
 }
+const StyledLinks = styled.a`
+    animation: ${float} 3s ease-in-out infinite ;
+`
 
